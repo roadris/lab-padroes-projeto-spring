e7492dc0118ac277885f1f02aa4910e44c0011fe
roadris <roadriss@gmail.com>
2024-07-08 23:44:22 -0300

    Atualizando a classe ClienteService
    Signed-off-by: roadris <roadriss@gmail.com>

--- src/main/java/one/digitalinnovation/gof/service/ClienteService.java
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -3,11 +3,11 @@ package one.digitalinnovation.gof.service;
 import one.digitalinnovation.gof.model.Cliente;
 
 /**
- * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
- * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * Interface que define o padrão comportamental <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter várias implementações dessa mesma
  * interface.
  * 
- * @author falvojr
+ * @author roadris
  */
 public interface ClienteService {
 
